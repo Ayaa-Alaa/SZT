2bfed46765de21f0a5b7ac117027ddcc5d5d8bef
ayaa & alaa <nahjanulahyar52@gmail.com>
2025-06-16 20:05:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function fetchLocalFile(filePath) {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, (err, data) => {
       if (err) return reject(err);
-    resolve(data);
+      resolve(data);
     });
   });
 }
